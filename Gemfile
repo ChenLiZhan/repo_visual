fd1c20d177d39125d2d76f6a1c7b672a49598188
chenlizhan <chung1350@hotmail.com>
2015-11-17 21:47:24 +0800

    Drilldown viz

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 gem 'mongo', '~> 2.1'
 gem 'chartkick', '~> 1.4', '>= 1.4.1'
 gem 'sinatra'
+gem 'httparty'
--- app.rb
+++ app.rb
@@ -1,11 +1,21 @@
 require 'sinatra/base'
 require 'chartkick'
 require 'mongo'
-
+require 'httparty'
+require 'date'
 
 class VizApp < Sinatra::Base
   client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'gems_info')
   
+  before do
+    documents = []
+    client[:gems].find.each do |document|
+      documents << document
+    end      
+
+    @doc = documents.last
+  end
+
   helpers do
     def version_downloads(data)
       version_downloads_hash = {}
@@ -14,6 +24,44 @@ class VizApp < Sinatra::Base
       end
 
       version_downloads_hash
+    end
+
+    def version_downloads_stack(data)
+      general_data = []
+      data.each do |row|
+        if general_data[row['number'].split('.').first.to_i].nil?
+          general_data[row['number'].split('.').first.to_i] = row['downloads']
+        else
+          general_data[row['number'].split('.').first.to_i] += row['downloads']
+        end
+      end
+
+      final_general = general_data.each_with_index.map do |row, index|
+        {
+          'name'    => "Version #{index}.*",
+          'y'       => row,
+          'drilldown' => "Version #{index}.*"
+        }
+      end
+
+      specific_data = []
+      data.each do |row|
+        if specific_data[row['number'].split('.').first.to_i].nil?
+          specific_data[row['number'].split('.').first.to_i] = []
+        else
+          specific_data[row['number'].split('.').first.to_i] << [row['number'], row['downloads']]
+        end
+      end
+
+      final_specific = specific_data.each_with_index.map do |row, index|
+        {
+          'name'  => "Version #{index}.*",
+          'id'    => "Version #{index}.*",
+          'data'  => row
+        }
+      end
+
+      [final_general, final_specific]
     end
 
     def commit_week_day(data)
@@ -39,6 +87,34 @@ class VizApp < Sinatra::Base
       commit_week_day
     end
 
+    # def commit_heatmap(data)
+    #   # cwday
+    #   # month
+
+    #   commits_transform = {
+    #     'data'  => [],
+    #     'max'   => 0
+    #   }
+    #   commits_transform['data'] << ['Date', 'Day', 'Commits']
+    #   data.each do |row|
+    #     stamp = row['week']
+    #     counter = 0
+    #     row['days'].each_with_index do |value, index|
+    #       commits_transform['max'] = value if value > commits_transform['max']
+
+    #       date = Date.strptime((row['week'] + 86400 * counter).to_s,'%s').to_s
+    #       commits_transform['data'] << [date, index, value]
+    #       counter += 1
+    #     end
+    #   end
+
+    #   commits_transform['data'] = commits_transform['data'].map do |row|
+    #     row.join(',')
+    #   end.join(' ')
+
+    #   commits_transform
+    # end
+
     def closed_issues(data)
       data.map! do |row|
         [row['number'], row['duration']]
@@ -48,17 +124,47 @@ class VizApp < Sinatra::Base
     end
   end
   
-  get '/' do
-    documents = []
-    client[:gems].find.each do |document|
-      documents << document
-    end      
-
-    @doc = documents.last
-
-    @version_downloads = version_downloads(@doc['version_downloads'])
-    @commit_week_day = commit_week_day(@doc['commit_activity_last_year'])
-    @closed_issues = closed_issues(@doc['closed_issues'])
-    erb :index
+  get '/rubygems' do
+    @version_downloads = HTTParty.get('http://localhost:4567/api/v1/rubygems/version_downloads')
+    @version_downloads_stack = HTTParty.get('http://localhost:4567/api/v1/rubygems/version_downloads_stack')
+    erb :rubygems
   end
+
+  get '/github' do
+    @closed_issues = HTTParty.get('http://localhost:4567/api/v1/github/closed_issues')
+    @commit_week_day = HTTParty.get('http://localhost:4567/api/v1/github/commit_week_day')
+    @commits_month_day = HTTParty.get('http://localhost:4567/api/v1/github/commits_month_day')
+    erb :github
+  end
+
+  get '/api/v1/rubygems/version_downloads' do
+    content_type :json
+    version_downloads = version_downloads(@doc['version_downloads'])
+    version_downloads.to_json
+  end
+
+  get '/api/v1/rubygems/version_downloads_stack' do
+    content_type :json
+    version_downloads_data = version_downloads_stack(@doc['version_downloads'])
+    version_downloads_data.to_json
+  end
+
+  get '/api/v1/github/closed_issues' do
+    content_type :json
+    closed_issues = closed_issues(@doc['closed_issues'])
+    closed_issues.to_json
+  end
+
+  get '/api/v1/github/commit_week_day' do
+    content_type :json
+    commit_week_day = commit_week_day(@doc['commit_activity_last_year'])
+    commit_week_day.to_json
+  end
+
+  # get '/api/v1/github/commits_month_day' do
+  #   content_type :json
+  #   commits = commit_heatmap(@doc['commit_activity_last_year'])
+  #   commits.to_json
+  # end
+
 end
