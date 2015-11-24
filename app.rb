835c3c4169568ad25de215cb3d83cf152c0a9341
chenlizhan <chung1350@hotmail.com>
2015-11-25 00:26:05 +0800

    Heatmap

--- app.rb
+++ app.rb
@@ -181,33 +181,49 @@ class VizApp < Sinatra::Base
       version_downloads_days
     end
 
-    # def commit_heatmap(data)
-    #   # cwday
-    #   # month
+    def commit_heatmap(data)
+      # p data
+      # cwday
+      # month
 
-    #   commits_transform = {
-    #     'data'  => [],
-    #     'max'   => 0
-    #   }
-    #   commits_transform['data'] << ['Date', 'Day', 'Commits']
-    #   data.each do |row|
-    #     stamp = row['week']
-    #     counter = 0
-    #     row['days'].each_with_index do |value, index|
-    #       commits_transform['max'] = value if value > commits_transform['max']
+      min_date = Date.strptime(data.first['week'].to_s, '%s').to_s
+      max_date = Date.strptime((data.last['week'] + 86400 * 6).to_s, '%s').to_s
 
-    #       date = Date.strptime((row['week'] + 86400 * counter).to_s,'%s').to_s
-    #       commits_transform['data'] << [date, index, value]
-    #       counter += 1
-    #     end
-    #   end
 
-    #   commits_transform['data'] = commits_transform['data'].map do |row|
-    #     row.join(',')
-    #   end.join(' ')
+      commits_transform = {
+        'data'  => [],
+        'max'   => 0,
+        'min_date' => min_date,
+        'max_date' => max_date
+      }
 
-    #   commits_transform
-    # end
+      date_index = 0
+      data.each do |row|
+        stamp = row['week']
+        counter = 0
+        row['days'].each_with_index do |value, index|
+          commits_transform['max'] = value if value > commits_transform['max']
+
+          date = Date.strptime((row['week'] + 86400 * counter).to_s,'%s')
+          
+          commits_transform['data'] << [date.month - 1, date.cwday % 7, value]
+          counter += 1
+          date_index += 1
+        end
+      end
+
+      commits_aggregate = Hash.new(0)
+      commits_transform['data'].each do |row|
+        commits_aggregate["#{row[0]}-#{row[1]}"] += row[2]
+      end
+
+      commits_transform['data'] = commits_aggregate.map do |row|
+        month, day = row[0].split('-')
+        [month.to_i, day.to_i, row[1]]
+      end
+
+      commits_transform
+    end
 
     def issues_info(data)
       data.map! do |row|
@@ -293,10 +309,10 @@ class VizApp < Sinatra::Base
     commit_week_day.to_json
   end
 
-  # get '/api/v1/github/commits_month_day' do
-  #   content_type :json
-  #   commits = commit_heatmap(@doc['commit_activity_last_year'])
-  #   commits.to_json
-  # end
+  get '/api/v1/github/commits_month_day' do
+    content_type :json
+    commits = commit_heatmap(@doc['commit_activity_last_year'])
+    commits.to_json
+  end
 
 end
