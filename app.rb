fd502990ba7f289f62f8a60463ea2895894f8b23
chenlizhan <chung1350@hotmail.com>
2016-03-15 22:51:10 +0800

    List gems

--- app.rb
+++ app.rb
@@ -31,13 +31,13 @@ class VizApp < Sinatra::Base
 
     all_gem_list = client[:gems].find({:last_commit => {'$gte' => 0}})
 
-    # all_gem_list.take(50).each do |document|
-    #   gem_list << {
-    #     '_id'         => document['_id'],
-    #     'name'        => document['name'],
-    #     'created_at'  => document['created_at']
-    #   }
-    # end      
+    all_gem_list.take(50).each do |document|
+      gem_list << {
+        '_id'         => document['_id'],
+        'name'        => document['name'],
+        'created_at'  => document['created_at']
+      }
+    end
 
     @doc = gem_list
     @total_count = all_gem_list.count
